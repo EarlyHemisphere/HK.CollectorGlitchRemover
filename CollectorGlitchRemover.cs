dea3e162399443eb9f932775afc1349da9554acd
Lucas Anderson <lucasanderson1999@gmail.com>
2024-08-14 22:17:47 -0700

    code

---
+++ CollectorGlitchRemover.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom;
+using HutongGames.PlayMaker;
+using Modding;
+using Satchel;
+
+namespace CollectorGlitchRemover {
+    public class CollectorGlitchRemover : Mod {
+        public static CollectorGlitchRemover instance;
+
+        public CollectorGlitchRemover() : base("Collector Glitch Remover") => instance = this;
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
+
+        public bool ToggleButtonInsideMenu => false;
+
+        public override void Initialize() {
+            Log("Initializing");
+
+            On.PlayMakerFSM.OnEnable += OnEnable;
+
+            Log("Initialized");
+        }
+
+        public void OnEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
+            orig(self);
+
+            if (self.gameObject.name == "Jar Collector" && self.FsmName == "Control") {
+                Modding.Logger.Log("Removing collector glitch");
+                self.RemoveAction("Hop Wall", 6);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# HK.CollectorGlitchRemover
+
+Removes the glitch in the collector fight where the collector does a grab attack without a preceding jump.
