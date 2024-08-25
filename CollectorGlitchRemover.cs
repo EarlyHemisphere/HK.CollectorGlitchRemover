199bd18cf0970b9685d688c4a313be076c8779fd
Lucas Anderson <lucasanderson1999@gmail.com>
2024-08-24 18:03:56 -0700

    actual fix for glitch

--- CollectorGlitchRemover.cs
+++ CollectorGlitchRemover.cs
@@ -1,9 +1,13 @@
 ﻿using Modding;
 using SFCore.Utils;
+using HutongGames.PlayMaker.Actions;
 
 namespace CollectorGlitchRemover {
     public class CollectorGlitchRemover : Mod {
         public static CollectorGlitchRemover instance;
+        private string prevPrevState = "";
+        private string prevState = "";
+        private int delay = 11;
 
         public CollectorGlitchRemover() : base("Collector Glitch Remover") => instance = this;
 
@@ -14,17 +18,36 @@ namespace CollectorGlitchRemover {
         public override void Initialize() {
             Log("Initializing");
 
-            On.PlayMakerFSM.OnEnable += OnEnable;
+            On.PlayMakerFSM.Update += OnUpdate;
 
             Log("Initialized");
         }
 
-        public void OnEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self) {
+        public void OnUpdate(On.PlayMakerFSM.orig_Update orig, PlayMakerFSM self) {
             orig(self);
 
-            if (self.gameObject.name == "Jar Collector" && self.FsmName == "Control") {
-                Modding.Logger.Log("Removing collector glitch");
-                self.RemoveAction("Hop Wall", 6);
+            if (self.FsmName == "Control" && self.gameObject.name == "Jar Collector") {
+                string curState = self.ActiveStateName;
+
+                if (prevPrevState == "Lunge Antic" && prevState == "Lunge Air" && curState == "Lunge Swipe") {
+                    // Remove landing trigger
+                    Modding.Logger.Log("Preventing collector insta-grab glitch");
+                    self.RemoveFsmTransition("Lunge Air", "LAND");
+                    delay = 0;
+                    prevPrevState = "";
+                    prevState = "";
+                    self.SetState("Lunge Launch");
+                } else if (delay < 10) {
+                    // Wait for launch to occur
+                    delay++;
+                } else if (delay == 10) {
+                    // After launch, add back landing trigger
+                    self.AddFsmTransition("Lunge Air", "LAND", "Lunge Swipe");
+                    delay++;
+                } else {
+                    prevPrevState = prevState;
+                    prevState = curState;
+                }
             }
         }
     }
